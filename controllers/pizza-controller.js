6a839134cc71ded5a47a5baff6ced2d8cacd7a1a
Charles <cchester1@angelo.edu>
2021-06-12 23:58:54 -0500

    ..

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -1,61 +1,63 @@
 const { Pizza } = require('../models');
 
 const pizzaController = {
-  //we are creating methods that will be used by routes in the controllers folder 
+  // get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
-    .then(results => res.json(results))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err)
-    })
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
   },
 
+  // get one pizza by id
   getPizzaById({ params }, res) {
-    //recall that we send data as an object to destructure it 
     Pizza.findOne({ _id: params.id })
-    .then(result => {
-      if(!result) {
-        res.status(404).json({ message: "None found" })
-        return;
-      }
-      res.json(result)
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    })
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
   },
 
+  // createPizza
   createPizza({ body }, res) {
     Pizza.create(body)
-    .then(result => res.json(result))
-    .catch(err => res.status(400).json(err))
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.json(err));
   },
 
+  // update pizza by id
   updatePizza({ params, body }, res) {
-    //must send { new: true } to the database to allow it to update 
     Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
-    .then(result => {
-      if (!result) {
-        res.status(404).json({ message: "None found" })
-        return;
-      }
-      res.json(result)
-    })
-    .catch(err => res.status(400).json(err))
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
   },
 
+  // delete pizza
   deletePizza({ params }, res) {
     Pizza.findOneAndDelete({ _id: params.id })
-    .then(result => {
-      if(!result) {
-        res.status(404).json({ message: "None found" })
-        return
-      }
-    })
-    .catch(err => res.status(400).json(err))
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.json(err));
   }
-}
+};
 
 module.exports = pizzaController;
--- server.js
+++ server.js
@@ -8,11 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('./routes'));
-
-//connect to process environment. if no process environment, connect to the local database (mongo will create it if it doesnt exist)
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
-  //configure the database rules 
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -20,5 +16,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
+
+app.use(require('./routes'));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
